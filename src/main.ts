c6db320af401622546844a542d2661ba033f9205
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-11 18:42:12 -0400

    dynamically-sized canvas

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,44 @@ import './style.scss';
 const easeljs = require('@createjs/easeljs');
 
 let body = document.getElementById('body');
-if (body !== null)
-	console.log('my body is ready');
 
-let stage = new easeljs.Stage('canvas');
+let stage: any = null;
 let circle = new easeljs.Shape();
-let text = new easeljs.Text('Tristan did the CoronaVirus!', '20px Arial', '#FFB6C1');
+let text = new easeljs.Text('benny is the bomb 💣💣💣', '20px Arial', '#FFB6C1');
 
 body.onload = function () {
+	console.log('my body is ready');
+
+	let canvas = document.body.appendChild(createCanvas());
+	stage = new easeljs.Stage(canvas);
+
 	circle.graphics.beginFill('Cyan').drawCircle(0, 0, 50);
-	circle.x = circle.y = 100;
+	circle.y = 300;
 	stage.addChild(circle);
 
 	text.x = stage.canvas.width;
+	text.y = 40;
 	text.textBaseline = 'top';
 	stage.addChild(text);
+}
+
+function createCanvas(): HTMLCanvasElement {
+	let canvas = document.createElement('canvas');
+
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+
+	if (width >= 500) {
+		width = 480;
+		height = 854;
+		// TODO: Do I *really* need to define border *here*? :(
+		canvas.style.border = '1px solid #000';
+	}
+
+	canvas.width = width;
+	canvas.height = height;
+
+	return canvas;
 }
 
 easeljs.Ticker.framerate = 60;
