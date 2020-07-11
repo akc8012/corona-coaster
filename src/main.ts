6eee4a270b161c58a35131a86e27f04058fe9c28
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-11 15:36:46 -0400

    Added moving text

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,25 @@ if (body !== null)
 	console.log('my body is ready');
 
 let stage = new easeljs.Stage('canvas');
-var circle = new easeljs.Shape();
+let circle = new easeljs.Shape();
+let text = new easeljs.Text('Tristan did the CoronaVirus!', '20px Arial', '#0000ff');
 
 body.onload = function () {
 	circle.graphics.beginFill('Cyan').drawCircle(0, 0, 50);
 	circle.x = circle.y = 100;
 	stage.addChild(circle);
+
+	text.x = stage.canvas.width;
+	text.textBaseline = 'top';
+	stage.addChild(text);
 }
 
 easeljs.Ticker.addEventListener('tick', function () {
 	circle.x += 10;
 	if (circle.x > stage.canvas.width)
 		circle.x = 0;
+
+	text.x -= 1;
 
 	stage.update();
 });
