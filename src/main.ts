f10ec75fdef802d30301585a225dac71429a9cf6
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-11 11:42:14 -0400

    init easeljs deps and setup canvas

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,7 @@
-let cheese: string = 'Sup world!';
-console.log(cheese);
+let body = document.getElementById('body');
+if (body !== null)
+	console.log('my body is ready');
+
+body.onload = function () {
+	console.log('its chewsday innit!');
+}
