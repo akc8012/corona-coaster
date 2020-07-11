709a04039af3bd3c0f31e85d262620636b83ad0a
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-11 15:26:52 -0400

    Circle moved!

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,19 @@ let body = document.getElementById('body');
 if (body !== null)
 	console.log('my body is ready');
 
-body.onload = function () {
-	let stage = new easeljs.Stage('canvas');
+let stage = new easeljs.Stage('canvas');
+var circle = new easeljs.Shape();
 
-	var circle = new easeljs.Shape();
+body.onload = function () {
 	circle.graphics.beginFill('Cyan').drawCircle(0, 0, 50);
 	circle.x = circle.y = 100;
 	stage.addChild(circle);
+}
+
+easeljs.Ticker.addEventListener('tick', function () {
+	circle.x += 10;
+	if (circle.x > stage.canvas.width)
+		circle.x = 0;
 
 	stage.update();
-
-	easeljs.Ticker.addEventListener('tick', handleTick);
-
-	function handleTick() {
-		circle.x += 10;
-
-		if (circle.x > stage.canvas.width)
-			circle.x = 0;
-
-		stage.update();
-	}
-}
+});
