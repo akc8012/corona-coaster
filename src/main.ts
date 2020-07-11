197f3ee7638b3a926f57dff91fd38e857883e98c
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-11 13:43:16 -0400

    easeljs working with basic stage

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+const easeljs = require('@createjs/easeljs');
+
 let body = document.getElementById('body');
 if (body !== null)
 	console.log('my body is ready');
 
 body.onload = function () {
-	console.log('its chewsday innit!');
+	let stage = new easeljs.Stage('canvas');
+	console.log(stage);
 }
