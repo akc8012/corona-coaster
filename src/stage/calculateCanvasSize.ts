70906a04698887cc0522d450cb52a39fb3fdcc0a
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-12 09:36:02 -0400

    fixed very clearly wrong method name

--- src/stage/calculateCanvasSize.ts
+++ src/stage/calculateCanvasSize.ts
@@ -3,12 +3,12 @@ const desktopSize = [480, 854];
 
 export function calculateCanvasSize(windowSize: number[]): CanvasSizeInfo {
 	const [width] = windowSize;
-	if (isMobileScreen(width))
+	if (isDesktopScreen(width))
 		return { size: desktopSize, needsBorder: true };
 
 	return { size: windowSize, needsBorder: false };
 }
 
-function isMobileScreen(width: number): boolean {
+function isDesktopScreen(width: number): boolean {
 	return width >= 500;
 }
