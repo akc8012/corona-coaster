b6a2d1da6421212357f1235e4395ebcb8d318f7a
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-12 18:13:56 -0400

    pow! pow pow! tween'd!

--- src/main.ts
+++ src/main.ts
@@ -2,44 +2,40 @@ const easeljs = require('@createjs/easeljs');
 const tweenjs = require('@createjs/tweenjs');
 
 import { createStage } from './stage/createStage';
-import './style.scss';
-
+import './assets/style.scss';
+// TODO: VS Code shows an error even though *its fine*
+import pow from './assets/sprites/pow.png';
 
 let stage: any = undefined;
-const circle = new easeljs.Shape();
 const text = new easeljs.Text('I am the tweeeeeeeeener!! 🥳🥳🥳', '20px Arial', '#FFB6C1');
 
 // TODO: resize on windowSizeChange event?
 document.getElementById('body').onload = function () {
 	stage = createStage();
 
-	circle.graphics.beginFill('Cyan').drawCircle(0, 0, 50);
-	circle.y = 100;
-	stage.addChild(circle);
+	let powBitmap = new easeljs.Bitmap(pow);
+	powBitmap.scale = 0.5;
+	powBitmap.x = -185;
+	powBitmap.y = 100;
+	stage.addChild(powBitmap);
 
-	tweenjs.Tween.get(circle, { loop: true })
-		.to({ x: 400, alpha: 1 }, 1000, tweenjs.Ease.getPowInOut(4))
+	tweenjs.Tween.get(powBitmap, { loop: true })
+		.to({ x: 235, alpha: 1 }, 1000, tweenjs.Ease.getPowInOut(4))
 		.to({ y: 250 }, 500, tweenjs.Ease.getPowInOut(2))
-		.to({ x: 100, alpha: 0 }, 800, tweenjs.Ease.getPowInOut(2));
-
-	tweenjs.Ticker.framerate = 60;
-	tweenjs.Ticker.addEventListener("tick", stage);
+		.to({ x: 0, alpha: 0 }, 800, tweenjs.Ease.getPowInOut(2));
 
 	text.x = stage.canvas.width;
 	text.y = 40;
 	text.textBaseline = 'top';
 	stage.addChild(text);
 
+	tweenjs.Ticker.framerate = 60;
+	tweenjs.Ticker.addEventListener("tick", stage);
 	console.log('my body is ready');
 }
 
 easeljs.Ticker.framerate = 60;
 easeljs.Ticker.addEventListener('tick', function () {
 	text.x -= 0.6;
-
-	circle.x += 5;
-	if (circle.x > stage.canvas.width)
-		circle.x = 0;
-
 	stage.update();
 });
