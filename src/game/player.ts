3f8e4c0574147297b0a264572f7f69532ca1589b
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-26 19:56:37 -0400

    Player has gravity, raycasts against track floor

--- src/game/player.ts
+++ src/game/player.ts
@@ -1,28 +1,54 @@
 import * as createjs from 'createjs-module';
-import { Bounds } from '../physics/raycast';
+import { Bounds, Colliders, Vector, raycast, Ray } from '../physics/raycast';
 import cart from '../assets/sprites/cart.png';
 
 
 export interface IPlayer {
 	bounds: Bounds;
-	update: () => void;
+	update: (track: Colliders) => void;
 }
 
+const GRAVITY = 2;
+
 export class Player implements IPlayer {
-	bounds = { x: 0, y: 0, width: 32, height: 32 };
+	bounds: Bounds;
+	vel: Vector = [0, 0];
 	sprite = new createjs.Bitmap(cart);
 
 	constructor(stage: createjs.Stage) {
+		this.bounds = {
+			x: ((stage.canvas as HTMLCanvasElement).width / 2) - 16,
+			y: 0,
+			width: 32,
+			height: 32
+		};
+
 		this.sprite.scaleX = this.bounds.width;
 		this.sprite.scaleY = this.bounds.height;
-		this.sprite.rotation = 22;
 
 		stage.addChild(this.sprite);
 	}
 
-	update() {
-		this.bounds.x += 0.1;
-		this.bounds.y += 0.05;
+	update(track: Colliders) {
+		this.vel[1] += GRAVITY;
+
+		const ray: Ray = {
+			origin: [this.bounds.x + (this.bounds.width / 2), this.bounds.y + (this.bounds.height / 2)],
+			maxDistance: (this.bounds.height / 2) + this.vel[1],
+		};
+
+		const hit = raycast(ray, track);
+		if (hit !== null) {
+			this.vel[1] = 0;
+			this.bounds.y = hit.point[1] - this.bounds.height;
+		}
+
+		this.updatePosition();
+	}
+
+	updatePosition() {
+		this.bounds.x += this.vel[0];
+		this.bounds.y += this.vel[1];
 
 		this.sprite.x = this.bounds.x;
 		this.sprite.y = this.bounds.y;
