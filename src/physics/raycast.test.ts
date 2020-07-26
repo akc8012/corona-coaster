6f657ca2decf0f448ba4857cc9ba7783ee2ddc2f
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-26 12:03:57 -0400

    Test formatting cleanup

--- src/physics/raycast.test.ts
+++ src/physics/raycast.test.ts
@@ -2,39 +2,27 @@ import { Ray, raycast, Hit } from './raycast';
 
 
 test('zero colliders returns no hit', () => {
-	const ray: Ray = {
-		origin: [2, 4],
-		maxDistance: 3,
-	};
+	const ray: Ray = { origin: [2, 4], maxDistance: 3 };
 
 	expect(raycast(ray, [])).toBeNull();
 });
 
 test('ray with short maxDistance returns no hit', () => {
-	const ray: Ray = {
-		origin: [-1.5, 1],
-		maxDistance: 0.5,
-	};
+	const ray: Ray = { origin: [-1.5, 1], maxDistance: 0.5 };
 	const bounds = { x: -3, y: 2, width: 4, height: 2 };
 
 	expect(raycast(ray, [bounds])).toBeNull();
 });
 
 test('ray outside x range returns no hit', () => {
-	const ray: Ray = {
-		origin: [-30, 0],
-		maxDistance: 3,
-	};
+	const ray: Ray = { origin: [-30, 0], maxDistance: 3 };
 	const bounds = { x: -4, y: 1, width: 10, height: 10 };
 
 	expect(raycast(ray, [bounds])).toBeNull();
 });
 
 test('ray intersection returns hit', () => {
-	const ray: Ray = {
-		origin: [1, 3],
-		maxDistance: 3,
-	};
+	const ray: Ray = { origin: [1, 3], maxDistance: 3 };
 
 	const bounds = { x: -2, y: 4, width: 6, height: 4 };
 	const expectedHit: Hit = { point: [1, 4], distance: 1 };
@@ -43,10 +31,7 @@ test('ray intersection returns hit', () => {
 });
 
 test('ray intersection returns hit past collider bounds', () => {
-	const ray: Ray = {
-		origin: [0, 0],
-		maxDistance: 5,
-	};
+	const ray: Ray = { origin: [0, 0], maxDistance: 5 };
 
 	const bounds = { x: -4, y: 1, width: 8, height: 1 };
 	const expectedHit: Hit = { point: [0, 1], distance: 1 };
@@ -55,10 +40,7 @@ test('ray intersection returns hit past collider bounds', () => {
 });
 
 test('ray intersection returns closest hit from several colliders', () => {
-	const ray: Ray = {
-		origin: [0, 0],
-		maxDistance: 5,
-	};
+	const ray: Ray = { origin: [0, 0], maxDistance: 5 };
 
 	const colliders = [
 		{ x: -4, y: 2, width: 8, height: 1 },
