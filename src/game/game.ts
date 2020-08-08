f2c0595e3dbeaa587528d178d4eb73e9fb2babd7
Andrew Colannino <akcolannino@email.neit.edu>
2020-08-08 11:50:18 -0400

    Refactor stage methods out of game.ts

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,7 +1,6 @@
 import * as createjs from 'createjs-module';
 
-import { createStage } from '../stage/createStage';
-import { addAnimation } from '../animation/tween';
+import { createStage, getCanvasSize } from '../stage/stage';
 import { Player, IPlayer } from './player';
 import { Colliders } from '../physics/raycast';
 import { ITrackPiece, TrackPiece } from './track';
@@ -9,43 +8,22 @@ import { ITrackPiece, TrackPiece } from './track';
 
 export function createGame() {
 	const stage = createStage();
-	addAnimation(stage);
 
 	const canvas = stage.canvas as HTMLCanvasElement;
 	const [width, height] = getCanvasSize(canvas);
-
-	const text = createText(width);
-	stage.addChild(text);
 
 	const track: Colliders = [
 		{ x: 0, y: 0, width, height: 32 },
 	];
 
 	const player: IPlayer = new Player(stage);
-
 	const twack: ITrackPiece = new TrackPiece({ x: 20, y: 20, width: 32, height: 32 }, stage);
 
 	createjs.Ticker.framerate = 60;
 	createjs.Ticker.addEventListener('tick', function () {
-		text.x -= 0.8;
 		track[0].y += 0.6;
 		player.update(track);
 
 		stage.update();
 	});
-}
-
-function getCanvasSize(canvas: HTMLCanvasElement): [number, number] {
-	return [canvas.width, canvas.height];
-}
-
-function createText(canvasWidth: number): createjs.Text {
-	const text = new createjs.Text('no longer looping the tween DEAL WITH IT', '20px Arial', '#FFB6C1');
-	text.name = 'text';
-
-	text.x = canvasWidth;
-	text.y = 50;
-	text.textBaseline = 'top';
-
-	return text;
 }
--- src/stage/stage.ts
+++ src/stage/stage.ts
@@ -1,15 +1,16 @@
 import * as createjs from 'createjs-module';
 import { calculateCanvasSize } from "./calculateCanvasSize";
+import { addAnimation } from '~/animation/tween';
 
-
-// TODO: put this in the style.scss, my guy
-const borderStyle = '1px solid #000';
 
 export function createStage(): createjs.Stage {
 	const canvas = createCanvas();
 	document.body.appendChild(canvas);
 
-	return new createjs.Stage(canvas);
+	const stage = new createjs.Stage(canvas);
+	addAnimation(stage);
+
+	return stage;
 }
 
 // TODO: resize on windowSizeChange event?
@@ -21,7 +22,11 @@ function createCanvas(): HTMLCanvasElement {
 	canvas.height = height;
 
 	if (needsBorder)
-		canvas.style.border = borderStyle;
+		canvas.style.border = '1px solid #000';
 
 	return canvas;
+}
+
+export function getCanvasSize(canvas: HTMLCanvasElement): [number, number] {
+	return [canvas.width, canvas.height];
 }
