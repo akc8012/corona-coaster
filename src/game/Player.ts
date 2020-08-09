bdf12b3a4b7510773f5b87f66636894f07478484
Andrew Colannino <akcolannino@email.neit.edu>
2020-08-09 19:45:20 -0400

    Add minecart dude sprite. Draw region and sprite separately with offset

--- src/game/Player.ts
+++ src/game/Player.ts
@@ -5,39 +5,56 @@ import { Vector, Size } from '../physics/math';
 import { ITrack } from './Track';
 import { getStageSize } from '~/stage/stage';
 
-import cart from '../assets/sprites/cart.png';
+import minecartDude from '../assets/sprites/MinecartDude.png';
 
 
 const GRAVITY = 2;
 const JUMP_HEIGHT = 22;
+
+const DRAW_REGION = false;
+
+const REGION_SIZE = [55, 32];
+const SPRITE_OFFSET = [-5, -30];
+const SPRITE_SCALE = 0.5;
 
 export interface IPlayer {
 	update: (track: ITrack) => void;
 }
 
 export class Player implements IPlayer {
+	// TODO: regions should be able to draw themselves?
 	region: Region;
-	vel: Vector = [0, 0];
-	sprite = new createjs.Bitmap(cart);
-	grounded = false;
+	regionRect: createjs.Shape;
+
+	vel: Vector;
+	sprite: createjs.Bitmap;
+	grounded: boolean;
 	stageSize: Size;
 
 	constructor(stage: createjs.Stage) {
+		this.vel = [0, 0];
+
 		const canvas = stage.canvas as HTMLCanvasElement;
+		this.createJumpEvents(canvas);
 		this.stageSize = getStageSize(stage);
 
-		const width = 32;
 		this.region = {
-			x: (canvas.width / 2) - (width / 2),
+			x: (canvas.width / 2) - (REGION_SIZE[0] / 2),
 			y: 0,
-			width,
-			height: width
+			width: REGION_SIZE[0],
+			height: REGION_SIZE[1]
 		};
 
-		this.sprite.scaleX = this.region.width;
-		this.sprite.scaleY = this.region.height;
+		this.regionRect = new createjs.Shape();
+		this.regionRect.graphics.beginFill('DeepSkyBlue').drawRect(
+			0, 0, this.region.width, this.region.height
+		);
 
-		this.createJumpEvents(canvas);
+		if (DRAW_REGION)
+			stage.addChild(this.regionRect);
+
+		this.sprite = new createjs.Bitmap(minecartDude);
+		this.sprite.scaleX = this.sprite.scaleY = SPRITE_SCALE;
 		stage.addChild(this.sprite);
 	}
 
@@ -115,7 +132,10 @@ export class Player implements IPlayer {
 		this.region.x += this.vel[0];
 		this.region.y += this.vel[1];
 
-		this.sprite.x = this.region.x;
-		this.sprite.y = this.region.y;
+		this.regionRect.x = this.region.x;
+		this.regionRect.y = this.region.y;
+
+		this.sprite.x = this.region.x + SPRITE_OFFSET[0];
+		this.sprite.y = this.region.y + SPRITE_OFFSET[1];
 	}
 }
