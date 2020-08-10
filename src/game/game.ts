4afaf972890533a3ae547377468d430dfb27687d
Andrew Colannino <akcolannino@email.neit.edu>
2020-08-09 20:58:03 -0400

    Add disclaimer text

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,12 +1,20 @@
 import * as createjs from 'createjs-module';
 
-import { createStage } from '../stage/stage';
+import { createStage, getStageSize } from '../stage/stage';
 import { Player, IPlayer } from './Player';
 import { ITrack, Track } from './Track';
 
 
 export function createGame() {
 	const stage = createStage();
+
+	const disclaimerText = new createjs.Text(
+		"this game isn't real yet\nplease don't hate me", '20px Arial', '#696969'
+	);
+	disclaimerText.x = getStageSize(stage).width / 2;
+	disclaimerText.y = 100;
+	disclaimerText.textAlign = 'center';
+	stage.addChild(disclaimerText);
 
 	const track: ITrack = new Track(stage);
 	const player: IPlayer = new Player(stage);
